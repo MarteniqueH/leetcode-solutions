cdf05976f755d69f5d650a20031c2c85f226a8d7
Martenique Harmon <155916707+MarteniqueH@users.noreply.github.com>
2026-09-03 19:13:40 -0400

    Time: 774 ms (5.03%), Space: 13.4 MB (88.99%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,14 +1,11 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        result = []
-        count = 0
 
         for num in nums:
             if num == 0:
-                count += 1
-            else:
-                result.append(num)
+                nums.remove(0)
+                nums.append(0)
+        return nums
 
-        result.extend([0] * count)
+       
 
-        nums[:] = result
